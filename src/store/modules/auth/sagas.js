610f615748badc262f589afb63b21ea4d2408eea
burn-c <chopcsom@gmail.com>
2020-03-22 19:25:59 -0300

    Config: Toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,5 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
+import { toast } from 'react-toastify';
+
 import history from '~/services/history';
 import api from '~/services/api';
 
@@ -20,6 +22,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, por favor, verificar seus dados!');
     yield put(signFailure());
   }
 }

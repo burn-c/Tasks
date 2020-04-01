27edaa6c835c8ca4928fef503b3718aa9fe272bc
burn-c <chopcsom@gmail.com>
2020-03-31 22:48:40 -0300

    FIX: createGlobalStyle

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');
+
 
 * {
     margin: 0;
